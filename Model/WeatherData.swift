e797aec6ff649fb28bf6cfcd5105b3d342307491
Andrea Rotunno <andrea.rotunno@outlook.it>
2023-01-23 16:39:02 +0100

    Fix file structure

---
+++ Model/WeatherData.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Andrea Rotunno on 23/01/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+struct Main: Decodable{
+    let temp: Double
+}
+struct Weather: Decodable {
+    let description: String
+}
---
+++ Model/WeatherManager.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Andrea Rotunno on 23/01/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=62cccc9e1c564c4378959025ddc38203&units=metric"
+
+func fetchWeather (cityName: String){
+    let urlString = "\(weatherURL)&q=\(cityName)"
+    performRequest(urlString)
+}
+
+func performRequest (_ urlString: String) {
+    if let url = URL(string: urlString) {
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            if let safeData = data {
+                parseJSON(weatherData: safeData)
+            }
+        }
+        task.resume()
+    }
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print("\(decodedData.name) \(decodedData.main.temp) \(decodedData.weather[0].description)" )
+        } catch {
+            print(error)
+        }
+    }
+    
+}
